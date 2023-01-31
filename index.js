b04efdf2e1d79050201c6216713e60eb6764af20
Christopher <chris.brower.96@gmail.com>
2023-01-31 10:12:35 -0700

    barely functional

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Write your solution in this
+var customerName = "bob";
+const leastFavoriteCustomer = 'bob'
+function upperCaseCustomerName() { 
+    //let upped = customerName.toUpperCase();
+    return customerName = customerName.toUpperCase(); 
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'lkj';
+    return leastFavoriteCustomer;
+}
